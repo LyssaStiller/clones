06b1741f92afc0cd22752aa5c26dc6fc45c09bc8
Lyssa Stiller <stiller108@gmail.com>
2019-04-02 20:52:39 -0400

    layout halfway there

---
+++ craigslist/script.js
@@ -0,0 +1,4 @@
+var community = ["activities", "artists", "childcare", "classes",
+"events",  "general", "groups", "local news", "lost+found",
+"missed connections", "musicians", "pets",
+"politics",  "rants & raves", "rideshare", "volunteers"]
